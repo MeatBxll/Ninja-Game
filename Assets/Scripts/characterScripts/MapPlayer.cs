a9594755fdccc8605edf89862ad452548b38896f
MeatBxll <anthonymancini27@gmail.com>
2025-08-09 23:06:01 -0400

    made map 1

---
+++ Assets/Scripts/characterScripts/MapPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MapPlayer : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector3(inputX, inputY, 0f) * moveSpeed;
+
+    }
+}
